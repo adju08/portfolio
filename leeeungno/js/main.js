15eaa91ba34eeb7991672b8f6b3821a82c57bc03
adju08 <haunje98@naver.com>
2025-07-24 09:35:39 +0900

    .

--- leeeungno/js/main.js
+++ leeeungno/js/main.js
@@ -2,19 +2,11 @@
 $(document).ready(function(){
     //console.log('들어가니')
 
-    window.onload = function () {
-        const vh = window.innerHeight;
-        AOS.init({
-            offset: vh < 800 ? 60 : 200,
-            duration: 500,
-            easing: 'ease',
-            once: false
-        });
-
-        setTimeout(() => {
-            AOS.refresh();
-        }, 300);
-    };
+    AOS.init({
+        offset: 200,
+        duration: 500,
+        easing: 'ease',
+    });
 
 
 /*************************** visual swiper 연결 : 시작 ***************************/
